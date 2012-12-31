b5f1027fc7e80dfbd9a7093746805bdfd6b57ef7
Aleksey V Zapparov <ixti@member.fsf.org>
2012-12-31 02:33:21 +0100

    Fix name of jekyll site getter

--- lib/jekyll/assets_plugin/site_patch.rb
+++ lib/jekyll/assets_plugin/site_patch.rb
@@ -26,15 +26,15 @@ module Jekyll
           @assets.css_compressor  = assets_config.css_compressor
 
           # bind jekyll and Sprockets context together
-          @assets.context_class.instance_variable_set :@jekyll_site, self
+          @assets.context_class.instance_variable_set :@site, self
 
           @assets.context_class.class_eval do
-            def jekyll_site
-              self.class.instance_variable_get :@jekyll_site
+            def site
+              self.class.instance_variable_get :@site
             end
 
             def asset_baseurl
-              jekyll_site.assets_config.baseurl.chomp "/"
+              site.assets_config.baseurl.chomp "/"
             end
 
             def asset_path(path, options = {})
@@ -42,8 +42,8 @@ module Jekyll
                 raise AssetFile::NotFound, "couldn't find file '#{path}'"
               end
 
-              unless jekyll_site.static_files.include? asset
-                jekyll_site.static_files << AssetFile.new(jekyll_site, asset)
+              unless site.static_files.include? asset
+                site.static_files << AssetFile.new(site, asset)
               end
 
               "#{asset_baseurl}/#{asset.digest_path}".squeeze "/"
