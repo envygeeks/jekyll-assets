53bdae447e42e4935b8a2004ef7e6075afad5486
Aleksey V Zapparov <ixti@member.fsf.org>
2013-02-10 06:13:18 +0100

    Fix tests

--- spec/lib/jekyll-assets/bourbon_spec.rb
+++ spec/lib/jekyll-assets/bourbon_spec.rb
@@ -5,10 +5,7 @@ require "jekyll-assets/bourbon"
 module Jekyll::AssetsPlugin
   describe "Bourbon integration" do
     it "should globally append bourbon paths into Sprockets environment" do
-      asset = @site.assets["vendor/bourbon.css"].to_s
-
-      asset.should =~ /-webkit-box-shadow/
-      asset.should =~ /box-shadow/
+      @site.assets["vendor/bourbon.css"].to_s.should =~ /linear-gradient/
     end
   end
 end
--- spec/lib/jekyll-assets/compass_spec.rb
+++ spec/lib/jekyll-assets/compass_spec.rb
@@ -5,11 +5,7 @@ require "jekyll-assets/compass"
 module Jekyll::AssetsPlugin
   describe "Compass integration" do
     it "should globally append compass paths into Sprockets environment" do
-      asset = @site.assets["vendor/compass.css"].to_s
-
-      asset.should =~ /-webkit-box-shadow/
-      asset.should =~ /-moz-box-shadow/
-      asset.should =~ /box-shadow/
+      @site.assets["vendor/compass.css"].to_s.should =~ /linear-gradient/
     end
   end
 end
