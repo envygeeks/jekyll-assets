f2864fc31d49f1301c7fe8f2dd15a724160b302f
Aleksey V Zapparov <ixti@member.fsf.org>
2013-02-16 19:15:49 +0100

    Update AssetFile specs

--- spec/lib/jekyll/assets_plugin/asset_file_spec.rb
+++ spec/lib/jekyll/assets_plugin/asset_file_spec.rb
@@ -4,9 +4,31 @@ require "spec_helper"
 module Jekyll::AssetsPlugin
   describe AssetFile do
     context "#destination" do
-      let(:file) { AssetFile.new(@site, @site.assets["app.css"]) }
-      subject { file.destination @dest.to_s }
-      it { should match %r{/app-[0-9a-f]{32}\.css$} }
+      subject do
+        AssetFile.new(@site, @site.assets["app.css"]).destination @dest.to_s
+      end
+
+      context "with none cachebust" do
+        before { @site.assets_config.cachebust = :none }
+        it { should match(%r{/app\.css$}) }
+      end
+
+      context "with soft cachebust" do
+        before { @site.assets_config.cachebust = :soft }
+        it { should match(%r{/app\.css$}) }
+      end
+
+      context "with hard cachebust" do
+        before { @site.assets_config.cachebust = :hard }
+        it { should match %r{/app-[0-9a-f]{32}\.css$} }
+      end
+
+      context "with unknown cachebust" do
+        before { @site.assets_config.cachebust = :wtf }
+        it "should raise error" do
+          expect { @site.asset_path "app.css" }.to raise_error
+        end
+      end
     end
   end
 end
