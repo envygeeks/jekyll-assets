11d71ba8edd8e1bd0d353f842decc52af1e08a35
Adam Michel <awmichel90@gmail.com>
2013-02-25 16:10:58 -0500

    Add neat integration.

---
+++ spec/lib/jekyll-assets/neat_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+require "jekyll-assets/neat"
+
+
+module Jekyll::AssetsPlugin
+  describe "Neat integration" do
+    it "should globally append neat paths into Sprockets environment" do
+      @site.assets["vendor/neat.css"].to_s.should =~ /max-width/
+    end
+  end
+end
