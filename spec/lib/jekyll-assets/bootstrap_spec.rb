f4ace97ee9c0ac650db4b1d54c3d659bbab30f11
Adilson Carvalho <lc.adilson@gmail.com>
2014-02-21 22:13:34 +0000

    Fixed the test

--- spec/lib/jekyll-assets/bootstrap_spec.rb
+++ spec/lib/jekyll-assets/bootstrap_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 require "jekyll-assets/bootstrap"
 
-
+require 'pry'
 module Jekyll::AssetsPlugin
   describe "Bootstrap integration" do
     it "should globally append bootstrap paths into Sprockets environment" do
-      @site.assets["vendor/bootstrap.css"].to_s.should =~ /bootstrap\//
+      @site.assets["bootstrap.css"].to_s.should =~ /bootstrap\//
     end
   end
 end
