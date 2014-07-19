a6bc115fcdfae83a3a8473e664d35830d044465e
Aleksey V Zapparov <ixti@member.fsf.org>
2014-07-19 20:31:05 +0200

    Fix bootstrap plugin
    Actually it just makes simplecov think that all lines are tested.
    I believe the best would be to write tests against paths that were added
    instead of integrational for bootstrap so that we could check which paths
    are added on different version (by stubbing gemspec.version message).
    
    But, I must admit I'm mostly don't care about these out-of-the-box plugins.
    And they are subjects of complete removeal in 1.0.0 release.

--- lib/jekyll-assets/bootstrap.rb
+++ lib/jekyll-assets/bootstrap.rb
@@ -1,10 +1,8 @@
 require "sprockets"
 
 gemspec = Gem::Specification.find_by_name "bootstrap-sass"
-%w[fonts javascripts stylesheets].each do |asset|
-  if Gem::Version.new("3.2") <= gemspec.version
-    Sprockets.append_path File.join(gemspec.gem_dir, "assets", asset)
-  else
-    Sprockets.append_path File.join(gemspec.gem_dir, "vendor", "assets", asset)
-  end
+subpath = Gem::Version.new("3.2") <= gemspec.version ? "" : "vendor"
+
+%w[images fonts javascripts stylesheets].each do |asset|
+  Sprockets.append_path File.join(gemspec.gem_dir, subpath, "assets", asset)
 end
