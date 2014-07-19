2f019150addae041b98fb4187891a657a92cc7a2
Aleksey V Zapparov <ixti@member.fsf.org>
2014-07-19 20:36:19 +0200

    Allow pass URIs to liquid tag/filter
    Related to #52

--- lib/jekyll/assets_plugin/renderer.rb
+++ lib/jekyll/assets_plugin/renderer.rb
@@ -5,42 +5,52 @@ module Jekyll
       JAVASCRIPT = '<script src="%s"></script>'
       IMAGE      = '<img src="%s">'
 
+      URI_RE     = %r{^(?:[^:]+:)?//(?:[^./]+\.)+[^./]+/}
+
       def initialize(context, logical_path)
         @site = context.registers[:site]
         @path = logical_path.strip
       end
 
       def render_asset
+        fail "Can't render remote asset: #{@path}" if remote?
         @site.assets[@path].to_s
       end
 
       def render_asset_path
+        return @path if remote?
         @site.asset_path @path
       end
 
       def render_javascript
-        @path << ".js" if File.extname(@path).empty?
-        render_tag JAVASCRIPT
+        render_tag JAVASCRIPT, ".js"
       end
 
       def render_stylesheet
-        @path << ".css" if File.extname(@path).empty?
-        render_tag STYLESHEET
+        render_tag STYLESHEET, ".css"
       end
 
       def render_image
         render_tag IMAGE
       end
 
-      protected
+      private
 
-      def render_tag(template)
+      def render_tag(template, extension = nil)
+        return format(template, @path) if remote?
+
+        @path << extension if extension && File.extname(@path).empty?
+
         asset = @site.assets[@path]
         tags  = (@site.assets_config.debug ? asset.to_a : [asset]).map do |a|
           format template, AssetPath.new(a).to_s
         end
 
         tags.join "\n"
+      end
+
+      def remote?
+        @path =~ URI_RE
       end
     end
   end
--- spec/lib/jekyll/assets_plugin/renderer_spec.rb
+++ spec/lib/jekyll/assets_plugin/renderer_spec.rb
@@ -14,8 +14,10 @@ RSpec.describe Jekyll::AssetsPlugin::Renderer do
 
   let(:renderer) do
     context = OpenStruct.new(:registers => { :site => site })
-    described_class.new context, "app"
+    described_class.new context, asset
   end
+
+  let(:asset) { "app" }
 
   describe "#render_javascript" do
     subject { renderer.render_javascript }
@@ -23,11 +25,21 @@ RSpec.describe Jekyll::AssetsPlugin::Renderer do
     context "when debug mode enabled" do
       let(:assets_config) { Hash[:debug, true] }
       it { is_expected.to match(/^(\s*<script src="[^"]+"><\/script>\s*){3}$/) }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/app.js" }
+        it { is_expected.to match(/^<script src="#{asset}"><\/script>$/) }
+      end
     end
 
     context "when debug mode disabled" do
       let(:assets_config) { Hash[:debug, false] }
       it { is_expected.to match(/^(\s*<script src="[^"]+"><\/script>\s*){1}$/) }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/app.js" }
+        it { is_expected.to match(/^<script src="#{asset}"><\/script>$/) }
+      end
     end
   end
 
@@ -37,12 +49,72 @@ RSpec.describe Jekyll::AssetsPlugin::Renderer do
     context "when debug mode enabled" do
       let(:assets_config) { Hash[:debug, true] }
       it { is_expected.to match(/^(\s*<link rel="stylesheet" [^>]+>\s*){3}$/) }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/style.css" }
+        it { is_expected.to match(/^<link rel="stylesheet" href="#{asset}">$/) }
+      end
     end
 
     context "when debug mode disabled" do
       let(:assets_config) { Hash[:debug, false] }
       it { is_expected.to match(/^(\s*<link rel="stylesheet" [^>]+>\s*){1}$/) }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/style.css" }
+        it { is_expected.to match(/^<link rel="stylesheet" href="#{asset}">$/) }
+      end
     end
   end
 
+  describe "#render_image" do
+    subject { renderer.render_image }
+
+    let(:asset) { "noise.png" }
+
+    context "when debug mode enabled" do
+      let(:assets_config) { Hash[:debug, true] }
+      it { is_expected.to match(%r{^<img src="/assets/noise-[^.]+\.png">$}) }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/style.css" }
+        it { is_expected.to match(/^<img src="#{asset}">$/) }
+      end
+    end
+
+    context "when debug mode disabled" do
+      let(:assets_config) { Hash[:debug, false] }
+      it { is_expected.to match(%r{^<img src="/assets/noise-[^.]+\.png">$}) }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/style.css" }
+        it { is_expected.to match(/^<img src="#{asset}">$/) }
+      end
+    end
+  end
+
+  describe "#render_asset" do
+    subject { renderer.render_asset }
+    let(:asset) { "alert.js" }
+
+    context "when debug mode enabled" do
+      let(:assets_config) { Hash[:debug, true] }
+      it { is_expected.to eq "alert('test');\n" }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/style.css" }
+        it { expect { subject }.to raise_error(/#{asset}/) }
+      end
+    end
+
+    context "when debug mode disabled" do
+      let(:assets_config) { Hash[:debug, false] }
+      it { is_expected.to eq "alert('test');\n" }
+
+      context "and URI given" do
+        let(:asset) { "http://example.com/style.css" }
+        it { expect { subject }.to raise_error(/#{asset}/) }
+      end
+    end
+  end
 end
