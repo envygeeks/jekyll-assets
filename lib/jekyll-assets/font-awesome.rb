8b45eadaa93e365e793ecc808a1bf77b3de552f1
Aleksey V Zapparov <ixti@member.fsf.org>
2014-09-14 23:08:21 +0200

    Support both old and new vresions of font-awesome

--- lib/jekyll-assets/font-awesome.rb
+++ lib/jekyll-assets/font-awesome.rb
@@ -1,6 +1,8 @@
 require "sprockets"
 
-fa = Gem::Specification.find_by_name("font-awesome-sass").gem_dir
+gemspec = Gem::Specification.find_by_name "font-awesome-sass"
+subpath = Gem::Version.new("4.2") <= gemspec.version ? "" : "vendor"
+
 %w[fonts stylesheets].each do |asset|
-  Sprockets.append_path File.join(fa, "assets", asset)
+  Sprockets.append_path File.join(gemspec.gem_dir, subpath, "assets", asset)
 end
