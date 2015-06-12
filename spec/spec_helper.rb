6e316db8fa4be3c3ef6f33960eb111b244f7bded
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2015-06-11 21:46:10 -0400

    Add spec focus filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,6 +42,8 @@ end
 RSpec.configure do |config|
   config.include FixturesHelpers
   config.extend  FixturesHelpers
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
   config.disable_monkey_patching!
 
