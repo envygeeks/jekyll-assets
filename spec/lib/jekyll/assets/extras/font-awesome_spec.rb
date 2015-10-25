1fab5fc8a1368e1c7a779515dbc1f0efebddee76
Jordon Bedwell <jordon@envygeeks.io>
2015-10-25 14:21:42 -0500

    font-awesome_spec clarification.

--- spec/lib/jekyll/assets/extras/font-awesome_spec.rb
+++ spec/lib/jekyll/assets/extras/font-awesome_spec.rb
@@ -4,7 +4,7 @@ describe "font-awesome" do
   let( :env) { Jekyll::Assets::Env.new(site) }
   let(:site) { stub_jekyll_site }
 
-  it "works" do
+  it "makes font-awesome available" do
     expect(env.find_asset("font-awesome").to_s).to match( \
       /fa-.+:before\s+{/)
   end
