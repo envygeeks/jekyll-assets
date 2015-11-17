4d4b9299b5f65c2621bc131a72a737684833c359
Jordon Bedwell <jordon@envygeeks.io>
2015-11-17 04:33:24 -0600

    Opportunistic cleanup.

--- spec/lib/jekyll/assets/liquid/tag/parser_spec.rb
+++ spec/lib/jekyll/assets/liquid/tag/parser_spec.rb
@@ -46,44 +46,54 @@ describe Jekyll::Assets::Liquid::Tag::Parser do
 
   it "raises an error if there is no proxy available" do
     input = "img.jpg sprockets:unknown:hello"
-    expect { subject.new(input, "img") }.to raise_error proxy_error
+    expect_it = expect { subject.new(input, "img") }
+    expect_it.to(raise_error(proxy_error))
   end
 
   it "makes like 'proxy' argument an HTML argument if there is no proxy key" do
     input = "img.jpg sprockets:unknown"
-    expect(subject.new(input, "img").args[:html]["sprockets"]).to eq "unknown"
+    expect(subject.new(input, "img").args[:html]["sprockets"]). \
+      to(eq("unknown"))
   end
 
   it "allows boolean proxy arguments", :proxies => true do
-    expect(subject.new("img.jpg magick:2x", "img").args[:magick][:"2x"]).to eq true
+    expect(subject.new("img.jpg magick:2x", "img").args[:magick][:"2x"]). \
+      to(eq(true))
   end
 
   it "does not allocate boolean arguments as proxy values", :proxies => true do
-    expect { subject.new("img.jpg magick:2x:raise", "img") }.to raise_error proxy_error
+    input = "img.jpg magick:2x:raise"
+    expect_it = expect { subject.new(input, "img") }
+    expect_it.to(raise_error(proxy_error))
   end
 
   it "expects escaping more than one colon with quotes" do
     input = "hello.jpg sprockets:accept:'image:gif'"
-    expect { subject.new(input, "img") }.to raise_error escape_error
+    expect_it = expect { subject.new(input, "img") }
+    expect_it.to(raise_error(escape_error))
   end
 
   it "expects escaping more than one colon without quotes" do
     input = "hello.jpg sprockets:acpet:image:gif"
-    expect { subject.new(input, "img") }.to raise_error escape_error
+    expect_it = expect { subject.new(input, "img") }
+    expect_it.to(raise_error(escape_error))
   end
 
   it "allows escaping inside of quotes" do
     input = 'hello.jpg sprockets:accept:"image\\\:gif"'
-    expect(subject.new(input, "img")[:sprockets][:accept]).to eq "image:gif"
+    expect(subject.new(input, "img")[:sprockets][:accept]). \
+      to(eq("image:gif"))
   end
 
   it "allows quoting the entire argument" do
     input = "hello.jpg 'sprockets:accept:image/gif'"
-    expect(subject.new(input, "img").args[:sprockets][:accept]).to eq "image/gif"
+    expect(subject.new(input, "img").args[:sprockets][:accept]). \
+      to(eq("image/gif"))
   end
 
   it "allows quoting only fragments of an argument" do
     input = "hello.jpg sprockets:accept:'image/gif'"
-    expect(subject.new(input, "img").args[:sprockets][:accept]).to eq "image/gif"
+    expect(subject.new(input, "img").args[:sprockets][:accept]). \
+      to(eq("image/gif"))
   end
 end
