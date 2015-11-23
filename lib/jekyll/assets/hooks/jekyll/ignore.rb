b34651482756aa60667c79cf5c17445cd8ab1fce
Jordon Bedwell <jordon@envygeeks.io>
2015-11-22 20:06:49 -0600

    Fix #212: Add forgotten size checks when grepping.

--- lib/jekyll/assets/hooks/jekyll/ignore.rb
+++ lib/jekyll/assets/hooks/jekyll/ignore.rb
@@ -3,7 +3,7 @@
 # Encoding: utf-8
 
 Jekyll::Hooks.register :site, :after_reset do |jekyll|
-  unless jekyll.config["exclude"].grep(/\A\.asset\-cache\/?/)
+  unless jekyll.config["exclude"].grep(/\A\.asset\-cache\/?/).size > 0
     jekyll.config["exclude"].push(".asset-cache/")
   end
 end
