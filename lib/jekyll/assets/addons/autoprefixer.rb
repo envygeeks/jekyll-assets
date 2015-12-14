f67f3588b56162f18248032f1d631d982ce8ec20
Dominik <dommmel@gmail.com>
2015-12-14 23:39:15 +0100

    Configure autoprefixer via asset_config
    E.g. (_config.yml):
     
        assets:
          autoprefixer:
              browsers: ["> 1%","last 4 versions","IE > 8"]

--- lib/jekyll/assets/addons/autoprefixer.rb
+++ lib/jekyll/assets/addons/autoprefixer.rb
@@ -4,6 +4,6 @@
 
 try_require_if_javascript "autoprefixer-rails" do
   Jekyll::Assets::Hook.register :env, :init do |env|
-    AutoprefixerRails.install(env)
+    AutoprefixerRails.install(env, env.asset_config["autoprefixer"])
   end
 end
