ae36183d3fd658785115ecc0d7b4c75882f41571
Jordon Bedwell <jordon@envygeeks.io>
2016-01-06 07:19:38 -0600

    Rearrange Jekyll/Assets.

--- lib/jekyll/assets.rb
+++ lib/jekyll/assets.rb
@@ -2,21 +2,25 @@
 # Copyright: 2012-2015 - MIT License
 # Encoding: utf-8
 
-require "tilt"
 require "sprockets"
 require "jekyll"
 
 module Jekyll
-  module Assets
-    require_relative "assets/version"
-    require_relative "assets/patches"
-    require_relative "assets/cached"
-    require_relative "assets/config"
-    require_relative "assets/env"
-    require_relative "assets/hook"
-    require_relative "assets/logger"
-    require_relative "assets/hooks"
-    require_relative "assets/liquid"
-    require_relative "assets/addons"
-  end
+  Assets = Module.new
 end
+
+require_relative "assets/env"
+require_relative "assets/version"
+require_relative "assets/patches"
+require_relative "assets/config"
+require_relative "assets/cached"
+require_relative "assets/hook"
+require_relative "assets/logger"
+require_relative "assets/hooks"
+require_relative "assets/liquid"
+require_relative "addons/bootstrap"
+require_relative "addons/autoprefixer"
+require_relative "addons/font_awesome"
+require_relative "addons/javascript"
+require_relative "processors/liquid"
+require_relative "proxies/magick"
