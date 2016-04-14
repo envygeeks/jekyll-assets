b9d6329cd1ec47fad64eea8aaefa93822f4d0daf
Jordon Bedwell <jordon@envygeeks.io>
2016-04-14 17:52:23 -0500

    Update copyright years.

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------------
 # Frozen-string-literal: true
-# Copyright: 2015 Jordon Bedwell - Apache v2.0 License
+# Copyright: 2015 Jordon Bedwell - MIT License
 # Encoding: utf-8
 # ----------------------------------------------------------------------------
 
