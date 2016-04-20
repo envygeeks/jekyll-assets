7496f7f2dfbfa54723804a51f8bf719a0ac0a945
Jordon Bedwell <jordon@envygeeks.io>
2016-04-19 20:22:54 -0500

    Copypasta failure.  I need to learn to copy my own source.

--- lib/jekyll/assets/addons/less.rb
+++ lib/jekyll/assets/addons/less.rb
@@ -4,8 +4,6 @@
 # Encoding: utf-8
 # ----------------------------------------------------------------------------
 
-try_require_if_javascript "jekyll/assets/processors/less" do
-  if ExecJS.runtime.is_a?(ExecJS::RubyRhinoRuntime)
-    Jekyll.logger.warn "ES6 transpiler has trouble with RubyRhino, use Node"
-  end
-end
+try_require_if_javascript(
+  "jekyll/assets/processors/less"
+)
