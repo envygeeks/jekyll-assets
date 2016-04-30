10652b9a70ec26f1174264c13b0f7d4ffe4c4119
Jordon Bedwell <jordon@envygeeks.io>
2016-04-30 00:04:12 -0500

    :gem: Release: v2.2.3

--- lib/jekyll/assets/version.rb
+++ lib/jekyll/assets/version.rb
@@ -1,11 +1,9 @@
-# ----------------------------------------------------------------------------
 # Frozen-string-literal: true
 # Copyright: 2012 - 2016 - MIT License
 # Encoding: utf-8
-# ----------------------------------------------------------------------------
 
 module Jekyll
   module Assets
-    VERSION="2.2.2"
+    VERSION="2.2.3"
   end
 end
