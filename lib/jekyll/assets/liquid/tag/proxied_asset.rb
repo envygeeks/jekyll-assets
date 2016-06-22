8a7a8f432fece3ce4f612aa2621be9ca0c073170
Saleem Rashid <spudowiar@outlook.com>
2016-06-22 21:13:39 +0100

    Proxies: implement mutable content type

--- lib/jekyll/assets/liquid/tag/proxied_asset.rb
+++ lib/jekyll/assets/liquid/tag/proxied_asset.rb
@@ -16,18 +16,21 @@ module Jekyll
 
           # ------------------------------------------------------------------
 
+          attr_reader :content_type
+
           def_delegator :@asset, :liquid_tags
-          def_delegator :@asset, :filename, :source_filename
-          def_delegator :@asset, :content_type
+          def_delegator :@asset, :source_filename
           def_delegator :@asset, :mtime
 
           # ------------------------------------------------------------------
 
           def initialize(asset, args, env, tag)
             @env = env
-            @asset = asset
+            @asset = asset.dup
             @args = args
             @tag = tag
+            @path = Pathutil.new(asset.logical_path)
+            @content_type = asset.content_type
 
             cache_file
             proxy_file
@@ -59,13 +62,15 @@ module Jekyll
           # ------------------------------------------------------------------
 
           def source
-            File.binread(filename)
+            filename.binread
           end
 
           # ------------------------------------------------------------------
 
           def filename
-            env.in_cache_dir(digest_path)
+            Pathutil.new(
+              env.in_cache_dir(digest_path)
+            )
           end
 
           # ------------------------------------------------------------------
@@ -74,6 +79,15 @@ module Jekyll
             Digest::SHA2.hexdigest(
               args.proxies.to_s
             )
+          end
+
+          # ------------------------------------------------------------------
+
+          def content_type=(type)
+            return if @content_type == type
+
+            @path = @path.sub_ext(".#{_ext_for(type)}")
+            @content_type = type
           end
 
           # ------------------------------------------------------------------
@@ -89,9 +103,23 @@ module Jekyll
           # ------------------------------------------------------------------
 
           def digest_path
-            name = asset.logical_path
-            ext  = File.extname(name)
-            "#{name.chomp(ext)}-#{digest}#{ext}"
+            "#{@path.sub_ext ""}-#{digest}#{@path.extname}"
+          end
+
+          # --------------------------------------------------------------------
+
+          private
+          def _mime_for(ext)
+            Sprockets.mime_exts[
+              ext
+            ]
+          end
+
+          # --------------------------------------------------------------------
+
+          private
+          def _ext_for(type)
+            Sprockets.mime_types[type][:extensions].first
           end
 
           # ------------------------------------------------------------------
@@ -100,7 +128,10 @@ module Jekyll
           def proxy_file
             unless cached?
               args.proxies.each do |key, val|
+                old_type = content_type
+                old_filename = filename
                 Proxies.get(key).first[:class].new(self, val, @args).process
+                old_filename.rm if old_type != content_type
               end
             end
           end
@@ -108,13 +139,24 @@ module Jekyll
           # ------------------------------------------------------------------
 
           private
-          def cache_file
-            @_cached = File.file?(filename)
-            return @_cached if @_cached
+          def find_cached
+            glob = filename.dirname.glob(filename.basename.sub_ext('.*'))
+            @_cached = filename.dirname.directory? && !glob.first.nil?
+            if @_cached
+              self.content_type = _mime_for(File.extname(glob.first))
+            end
 
-            dir = File.dirname(filename)
-            FileUtils.mkdir_p dir unless dir == env.in_cache_dir
-            FileUtils.cp asset.filename, filename
+            @_cached
+          end
+
+          # ------------------------------------------------------------------
+
+          private
+          def cache_file
+            return true if @_cached || find_cached
+
+            filename.dirname.mkdir_p unless filename.dirname == env.in_cache_dir
+            Pathutil.new(asset.filename).cp filename
             true
           end
         end
