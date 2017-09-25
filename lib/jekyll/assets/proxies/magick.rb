578092b7145217094881e7f04c05e84cc3e2fec2
Jordon Bedwell <jordon@envygeeks.io>
2017-09-25 00:44:52 -0500

    Add strip to the list of allowed args.

--- lib/jekyll/assets/proxies/magick.rb
+++ lib/jekyll/assets/proxies/magick.rb
@@ -5,7 +5,7 @@
 # ----------------------------------------------------------------------------
 
 try_require "mini_magick" do
-  MINI_MAGICK_ARGS = %W(resize quality rotate crop flip format gravity).freeze
+  MINI_MAGICK_ARGS = %W(resize quality rotate crop flip format gravity strip).freeze
   MINI_MAGICK_PRESETS = %W(@2x @4x @1/2 @1/3 @2/3 @1/4 @2/4 @3/4
     @double @quadruple @half @one-third @two-thirds @one-fourth
       @two-fourths @three-fourths).freeze
