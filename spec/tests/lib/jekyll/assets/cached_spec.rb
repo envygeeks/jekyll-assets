d68ad832b22eb2b1f98635bc43c0032e02a5c0fc
Jordon Bedwell <jordon@envygeeks.io>
2017-09-30 17:49:37 -0500

    Add Jekyll::Assets::Cached specs.

---
+++ spec/tests/lib/jekyll/assets/cached_spec.rb
@@ -0,0 +1,19 @@
+require "rspec/helper"
+describe Jekyll::Assets::Cached do
+  let(:env) { Jekyll::Assets::Env.new(site) }
+  before(:each, :process => true) { site.process }
+  let(:path) { site.in_dest_dir("/assets") }
+  let(:site) { stub_jekyll_site }
+  subject { env }
+
+  it { respond_to(:manifest) }
+  it { respond_to(:asset_config) }
+  it { respond_to(:uncached) }
+  it { respond_to(:jekyll) }
+
+  describe "#uncached" do
+    it "should be the environment" do
+      expect(env.cached.uncached).to(eq(env))
+    end
+  end
+end
