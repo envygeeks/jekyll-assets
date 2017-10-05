4d23a15e8c798e1b2d77375dd8101c3f31048cd6
Jordon Bedwell <jordon@envygeeks.io>
2017-10-05 16:35:37 -0500

    It's too much work to do all that trash.

--- lib/jekyll/assets.rb
+++ lib/jekyll/assets.rb
@@ -2,26 +2,22 @@
 # Copyright: 2012 - 2017 - MIT License
 # Encoding: utf-8
 
+# --
+# External
+# --
 require "sprockets"
 require "extras/all"
 require "jekyll/sanity"
 require "pathutil"
 require "jekyll"
+require "liquid"
 
-# --
-require "jekyll/assets/patches/obsolete_files"
-require "jekyll/assets/patches/sprockets_data_uri"
-require "jekyll/assets/patches/sprockets"
-require "jekyll/assets/patches/kernel"
+def require_all(*globs)
+  path = Pathutil.new("assets").expand_path(__dir__)
+  globs.each { |v| path.glob(v).reject { |o| o.directory? }.each do |vv|
+    require vv
+  end }
+end
 
-# --
-require "jekyll/assets/version"
-require "jekyll/assets/hooks/drops"
-require "jekyll/assets/hooks/setup"
-require "jekyll/assets/hook"
-require "jekyll/assets/cached"
-require "jekyll/assets/helpers"
-require "jekyll/assets/config"
-require "jekyll/assets/logger"
-require "jekyll/assets/proxy"
-require "jekyll/assets/env"
+require_all "patches/*", "*", "liquid/*",
+    "hooks/*", "plugins/*"
