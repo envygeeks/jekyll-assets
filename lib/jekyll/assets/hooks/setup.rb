7925cfa37e48761414b6db4704dfd9a6912b68f5
Jordon Bedwell <jordon@envygeeks.io>
2017-10-10 09:47:01 -0500

    Use `post_read`

--- lib/jekyll/assets/hooks/setup.rb
+++ lib/jekyll/assets/hooks/setup.rb
@@ -2,6 +2,6 @@
 # Copyright: 2012 - 2017 - MIT License
 # Encoding: utf-8
 
-Jekyll::Hooks.register :site, :after_reset do |o|
+Jekyll::Hooks.register :site, :post_read do |o|
   Jekyll::Assets::Env.new(o)
 end
