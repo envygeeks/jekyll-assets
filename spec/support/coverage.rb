f14b8330bb89e13ccb988ff2037aca0bd8db9167
Jordon Bedwell <jordon@envygeeks.io>
2017-10-10 22:47:29 -0500

    No need for that rescue.

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -2,10 +2,5 @@
 # Copyright: 2015 Jordon Bedwell - MIT License
 # Encoding: utf-8
 
-begin
-  require 'simplecov'
-  SimpleCov.start
-
-rescue LoadError
-  nil
-end
+require 'simplecov'
+SimpleCov.start
