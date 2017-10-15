688d3af614807c08d1583ec210d24f9b28b84f15
Jordon Bedwell <jordon@envygeeks.io>
2017-10-15 06:48:41 -0500

    Forgot to switch a file to using `environment.`

--- spec/tests/lib/jekyll/assets/env_spec.rb
+++ spec/tests/lib/jekyll/assets/env_spec.rb
@@ -11,14 +11,14 @@ describe Jekyll::Assets::Env do
 
   describe "#manifest" do
     it "should put the path in Jekyll's dest dir" do
-      expect(env.manifest.path).to(start_with(env.jekyll.source))
+      expect(environment.manifest.path).to(start_with(environment.jekyll.source))
     end
   end
 
   describe "#to_liquid_payload" do
     it "should build a list of liquid drops" do
-      expect(env.to_liquid_payload).to(be_a(Hash))
-      env.to_liquid_payload.each do |_, v|
+      expect(environment.to_liquid_payload).to(be_a(Hash))
+      environment.to_liquid_payload.each do |_, v|
         expect(v).to(be_a(Jekyll::Assets::Drop))
       end
     end
@@ -32,12 +32,12 @@ describe Jekyll::Assets::Env do
         ]
       })
 
-      env.send(:precompile!)
+      environment.send(:precompile!)
     end
 
-    let(:asset) { env.find_asset!("img.png") }
+    let(:asset) { environment.find_asset!("img.png") }
     it "should compile those extra assets" do
-      expect(Pathutil.new(env.in_dest_dir(asset.digest_path))).to(exist)
+      expect(Pathutil.new(environment.in_dest_dir(asset.digest_path))).to(exist)
     end
   end
 
@@ -45,7 +45,7 @@ describe Jekyll::Assets::Env do
     context "when cdn? = true" do
       context "and when cdn.baseurl = true" do
         before do
-          env.instance_variable_set(:@baseurl, nil)
+          environment.instance_variable_set(:@baseurl, nil)
           stub_asset_config({
             cdn: {
               baseurl: true
@@ -59,13 +59,13 @@ describe Jekyll::Assets::Env do
         end
 
         it "should add baseurl" do
-          expect(env.baseurl).to(eq("hello"))
+          expect(environment.baseurl).to(eq("hello"))
         end
       end
 
       context "and when cdn.baseurl = false" do
         before do
-          env.instance_variable_set(:@baseurl, nil)
+          environment.instance_variable_set(:@baseurl, nil)
           stub_asset_config({
             cdn: {
               baseurl: false
@@ -79,14 +79,14 @@ describe Jekyll::Assets::Env do
         end
 
         it "doesn't add baseurl" do
-          expect(env.baseurl).to(eq(""))
+          expect(environment.baseurl).to(eq(""))
         end
       end
 
       context "and when cdn.prefix = true" do
         before do
           allow(env).to(receive(:cdn?).and_return(true))
-          env.instance_variable_set(:@baseurl, nil)
+          environment.instance_variable_set(:@baseurl, nil)
           stub_asset_config({
             cdn: {
               prefix: true
@@ -95,14 +95,14 @@ describe Jekyll::Assets::Env do
         end
 
         it "should add the prefix" do
-          expect(env.baseurl).to(eq("/assets"))
+          expect(environment.baseurl).to(eq("/assets"))
         end
       end
 
       context "and when cdn.prefix = false" do
         before do
           allow(env).to(receive(:cdn?).and_return(true))
-          env.instance_variable_set(:@baseurl, nil)
+          environment.instance_variable_set(:@baseurl, nil)
           stub_asset_config({
             cdn: {
               prefix: false
@@ -111,7 +111,7 @@ describe Jekyll::Assets::Env do
         end
 
         it "doesn't add prefix" do
-          expect(env.baseurl).to(eq(""))
+          expect(environment.baseurl).to(eq(""))
         end
       end
     end
@@ -126,7 +126,7 @@ describe Jekyll::Assets::Env do
       end
 
       it "should add the baseurl" do
-        expect(env.prefix_path).to(eq("hello/assets"))
+        expect(environment.prefix_path).to(eq("hello/assets"))
       end
     end
 
@@ -142,12 +142,12 @@ describe Jekyll::Assets::Env do
       end
 
       it "should add the cdn url" do
-        expect(env.prefix_path).to(eq("hello.world/assets"))
+        expect(environment.prefix_path).to(eq("hello.world/assets"))
       end
     end
 
     it "should add paths" do
-      expect(env.prefix_path("hello.world")).to(eq("/assets/hello.world"))
+      expect(environment.prefix_path("hello.world")).to(eq("/assets/hello.world"))
     end
   end
 end
