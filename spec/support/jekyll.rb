7d302e2fa162426c5361c931cb6e253c8cf33c3e
Jordon Bedwell <jordon@envygeeks.io>
2017-10-15 06:52:56 -0500

    Cleanup our spec helpers.

--- spec/support/jekyll.rb
+++ spec/support/jekyll.rb
@@ -2,58 +2,67 @@
 # Copyright: 2012 - 2017 - MIT License
 # Encoding: utf-8
 
+require "forwardable/extended"
 require "nokogiri"
 
 module Jekyll
   module RSpecHelpers
+    extend Forwardable::Extended
+    rb_delegate :stub_jekyll_site, to: :"Jekyll::RSpecHelpers"
+    rb_delegate :stub_environment, to: :"Jekyll::RSpecHelpers"
+
     def fragment(html)
       Nokogiri::HTML.fragment(html)
     end
 
-    def self.silence_stdout()
-      old_stdout = $stdout; old_stderr = $stderr
+    def self.silence_stdout
+      stdout, stderr = $stdout, $stderr
       $stdout = StringIO.new
       $stderr = StringIO.new
+
       yield
     ensure
-      $stdout = old_stdout
-      $stderr = old_stderr
+      $stdout = stdout
+      $stderr = stderr
     end
 
     def stub_jekyll_config(hash)
       hash = hash.deep_stringify_keys
-      hash = Jekyll::Utils.deep_merge_hashes(jekyll.config, hash)
-      allow(jekyll).to(receive(:config).and_return(hash))
+      hash = jekyll.config.deep_merge(hash)
+      allow(jekyll).to(receive(:config).
+        and_return(hash))
     end
 
     def stub_asset_config(hash)
-      allow(env).to(receive(:asset_config).and_return(
-        env.asset_config.deep_merge(hash)))
-    end
-
-    def stub_jekyll_site(*args)
-      RSpecHelpers.stub_jekyll_site(*args)
+      hash = environment.asset_config.deep_merge(hash)
+      allow(env).to(receive(:asset_config).
+        and_return(hash))
     end
 
     def self.cleanup_trash
       %W(.jekyll-metadata _site .jekyll-cache).each do |v|
-        FileUtils.rm_rf(File.join(fixture, v))
+        Pathutil.new(fixture_path).join(v).rm_rf
       end
     end
 
     def self.stub_jekyll_site(opts = {})
       @jekyll ||= begin
         silence_stdout do
-          dest = File.join(fixture, "_site")
-          opts = Jekyll.configuration("destination" => dest, "source" => fixture)
-          Jekyll::Site.new(opts).tap(&:reset).
-            tap(&:read).tap(&:process).
-            tap(&:render)
+          dest = File.join(fixture_path, "_site")
+          Jekyll::Site.new(Jekyll.configuration({
+            "destination" => dest, "source" => fixture_path
+          })).tap(&:process)
         end
       end
     end
 
-    def self.fixture
+    def self.stub_environment
+      @environment ||= begin
+        Jekyll::Assets::Env.new(stub_jekyll_site)
+      end
+    end
+
+    def self.fixture_path
       File.expand_path("../fixture", __dir__)
     end
   end
@@ -63,7 +72,7 @@ module Jekyll
 
     let(:sprockets) { env }
     subject { described_class }
-    let(:env) { Jekyll::Assets::Env.new(jekyll) }
+    let(:environment) { stub_environment }
     let(:site) { stub_jekyll_site }
     let(:jekyll) { site }
   end
@@ -72,7 +81,7 @@ end
 # --
 
 RSpec.configure do |c|
-  c.after (:suite) {  Jekyll::RSpecHelpers.cleanup_trash }
+  # c.after (:suite) {  Jekyll::RSpecHelpers.cleanup_trash }
   c.before(:suite) do Jekyll::RSpecHelpers.cleanup_trash
     Jekyll::RSpecHelpers.stub_jekyll_site
   end
