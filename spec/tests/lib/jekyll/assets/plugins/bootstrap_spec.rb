31154ef717c7d7129bf5dd7435b2559082fa6f68
Jordon Bedwell <jordon@envygeeks.io>
2017-10-20 13:47:03 -0500

    Fix our tests, Bootstrap made some changes.

--- spec/tests/lib/jekyll/assets/plugins/bootstrap_spec.rb
+++ spec/tests/lib/jekyll/assets/plugins/bootstrap_spec.rb
@@ -6,6 +6,6 @@ require "rspec/helper"
 describe "Plugins/Bootstrap" do
   let(:asset) { env.find_asset!("plugins/botstrap.css") }
   it "should require bootstrap" do
-    expect(asset.to_s).to(match(/^html\{/))
+    expect(asset.to_s).to(match("Bootstrap"))
   end
 end
