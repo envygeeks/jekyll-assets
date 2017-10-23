1861107562e55a30c961172b51924e71d7ec945d
Jordon Bedwell <jordon@envygeeks.io>
2017-10-23 13:55:03 -0500

    Beef up our `obsolete_files` patches, this way we don't remove our data.

--- lib/jekyll/assets/patches/obsolete_files.rb
+++ lib/jekyll/assets/patches/obsolete_files.rb
@@ -7,18 +7,54 @@ module Jekyll
     module Patches
       module ObsoleteFiles
         def obsolete_files
-          sprockets, manifest = site.sprockets, site.sprockets.manifest
-          reject = manifest.files.keys.map do |v|
-            asset = sprockets.in_dest_dir(v)
-            [asset+".gz",asset+".map",asset]
+          extra = manifest_files.tap(&:flatten!)
+          super.reject do |v|
+            v == site.sprockets.in_dest_dir || \
+            v == site.sprockets.manifest.filename || \
+            extra.include?(v)
+          end
+        end
+
+        # --
+        # Get all the manifest files.
+        # @note this includes dynamic keys, like SourceMaps.
+        # @return [Array<String>]
+        # --
+        def manifest_files
+          manifest = site.sprockets.manifest.data
+          out = (manifest.keys - %w(files)).map do |v|
+            v = manifest[v]
+            ret = nil
+
+            if v.is_a?(Hash)
+              ret = v.map do |k, vv|
+                vv = site.sprockets.in_dest_dir(vv)
+                k  = site.sprockets.in_dest_dir( k)
+
+                [
+                  vv,
+                  vv + ".gz",
+                  File.dirname(vv),
+                  File.dirname(k),
+                  k + ".gz",
+                  k,
+                ]
+              end
+            elsif v.is_a?(Array)
+              ret = v.map do |k|
+                k = site.sprockets.in_dest_dir(k)
+                [
+                  k + ".gz",
+                  File.dirname(k),
+                  k,
+                ]
+              end
+            end
+
+            ret
           end
 
-          reject.flatten!
-          super.reject do |v|
-            v == manifest.filename || \
-            v == sprockets.in_dest_dir || \
-            reject.include?(v)
-          end
+          out.flatten.compact.uniq
         end
       end
     end
