014272e58218b0faa287a4031067f347b17137f0
Jordon Bedwell <jordon@envygeeks.io>
2017-11-29 01:48:11 -0600

    Rearrange our patches.

---
+++ lib/jekyll/assets/patches/asset.rb
@@ -0,0 +1,25 @@
+# Frozen-string-literal: true
+# Copyright: 2012 - 2017 - MIT License
+# Encoding: utf-8
+
+module Jekyll
+  module Assets
+    module Patches
+      module Sprockets
+        module Asset
+          def data_uri
+            "data:#{content_type};base64,#{Rack::Utils.escape(
+              Base64.encode64(to_s))}"
+          end
+        end
+      end
+    end
+  end
+end
+
+# --
+module Sprockets
+  class Asset
+    prepend Jekyll::Assets::Patches::Sprockets::Asset
+  end
+end
