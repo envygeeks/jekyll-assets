76b4f20e401c752f66f4e083c29924ab71198ef6
Jordon Bedwell <jordon@envygeeks.io>
2017-12-13 14:50:32 -0600

    Add a priority to our hooks.

--- lib/jekyll/assets.rb
+++ lib/jekyll/assets.rb
@@ -18,12 +18,18 @@ def require_all(*globs)
 end
 
 require_relative "assets/env"
-Jekyll::Hooks.register(:site, :post_read) do |o|
+Jekyll::Hooks.register :site, :post_read, priority: 99 do |o|
   unless o.sprockets
     Jekyll::Assets::Env.new(o)
   end
 end
 
-Jekyll::Hooks.register :site, :post_render do |o|
+# --
+# Post render hook.
+# We need to run really early because we want to have our
+#   stuff block and be done just incase something else relies
+#   on our stuff to do their stuff.  Such as reloaders.
+# --
+Jekyll::Hooks.register :site, :post_render, priority: 99 do |o|
   o&.sprockets&.write_all
 end
