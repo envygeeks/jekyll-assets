ea004041683329cedbd81f39934cc06473959c7d
Jordon Bedwell <jordon@envygeeks.io>
2018-03-12 11:56:33 -0500

    Make sure our compressor is ran, the hooks are necessary.

--- lib/jekyll/assets/compressors/uglify.rb
+++ lib/jekyll/assets/compressors/uglify.rb
@@ -16,20 +16,14 @@ module Jekyll
         end
       end
 
-      Hook.register :env, :before_init, priority: 1 do |e|
-        uglifier_config = e.asset_config[:compressors][:uglifier].symbolize_keys
-
-        Sprockets.register_compressor "application/javascript",
-          :assets_uglify,
-          Uglify.new(uglifier_config)
-      end
-
+      # rubocop:disable Metrics/LineLength
+      Sprockets.register_compressor "application/javascript", :assets_uglify, Uglify
       Hook.register :env, :after_init, priority: 3 do |e|
-        e.js_compressor = nil
         next unless e.asset_config[:compression]
-        if Utils.javascript?
-          e.js_compressor = :assets_uglify
-        end
+
+        e.js_compressor = nil unless Utils.javascript?
+        config = e.asset_config[:compressors][:uglifier].symbolize_keys
+        e.js_compressor = Uglify.new(config) if Utils.javascript?
       end
     end
   end
