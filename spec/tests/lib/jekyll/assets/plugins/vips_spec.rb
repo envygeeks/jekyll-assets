05769a841eb4c8640f65404e9da79958cac5d412
Andrew Heberle <andrew.heberle@gmail.com>
2019-06-14 23:28:58 +0800

    Add tests for half and double

--- spec/tests/lib/jekyll/assets/plugins/vips_spec.rb
+++ spec/tests/lib/jekyll/assets/plugins/vips_spec.rb
@@ -51,4 +51,36 @@ describe "Plugins/Vips" do
       expect(h).to(eq(100))
     end
   end
+
+  describe "double" do
+    let :page do
+      jekyll.pages.find do |v|
+        v.path == "plugins/vips/double.html"
+      end
+    end
+
+    it "should be doubled" do
+      frag = fragment(page.to_s).children.first
+      w1, h1 = FastImage.size(asset.filename)
+      w2, h2 = FastImage.size(env.jekyll.in_dest_dir(frag.attr("src")))
+      expect(w2).to(eq(w1 * 2))
+      expect(h2).to(eq(h1 * 2))
+    end
+  end
+
+  describe "half" do
+    let :page do
+      jekyll.pages.find do |v|
+        v.path == "plugins/vips/half.html"
+      end
+    end
+
+    it "should be halved" do
+      frag = fragment(page.to_s).children.first
+      w1, h1 = FastImage.size(asset.filename)
+      w2, h2 = FastImage.size(env.jekyll.in_dest_dir(frag.attr("src")))
+      expect(w2).to(eq(w1 * 0.5))
+      expect(h2).to(eq(h1 * 0.5))
+    end
+  end
 end
